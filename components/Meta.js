a5de2d57a938d6c1d118af38aefaec1ca758fb4a
Tyler <tyler.wellss@gmail.com>
2019-01-28 19:17:24 +0800

    add description for google crawler

--- components/Meta.js
+++ components/Meta.js
@@ -9,6 +9,7 @@ export default class Meta extends Component{
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <title>{this.props.title ? this.props.title : 'ESL Bot Jobs Aggregator'}</title>
+        <meta property="og:description" content="An ESL jobs aggregator. Search ESL teaching jobs in China, Japan, Vietnam, and worldwide."/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
       </Head>
